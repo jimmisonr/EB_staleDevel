10a899cb5090c42852589696a3da4e5ff4a07c91
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2014-11-11 14:55:56 +0700

    Fix CB plugin, makes it works with latest version of Events Booking

--- plugins/eventbooking/cb/cb.php
+++ plugins/eventbooking/cb/cb.php
@@ -17,6 +17,7 @@ class plgEventBookingCB extends JPlugin
 	{
 		
 		parent::__construct($subject, $config);
+		$this->canRun = file_exists(JPATH_ROOT . '/components/com_comprofiler/comprofiler.php');
 	}
 
 	/**
@@ -25,12 +26,12 @@ class plgEventBookingCB extends JPlugin
 	 */
 	function onAfterStoreRegistrant($row)
 	{
-		if (!file_exists(JPATH_ROOT . '/components/com_comprofiler/comprofiler.php'))
+		if (!$this->canRun)
 		{
 			return;
 		}
-		$config = EventBookingHelper::getConfig();
-		$integration = (int) $config->cb_integration;
+		require_once JPATH_ROOT . '/components/com_eventbooking/helper/helper.php';
+		$integration = EventBookingHelper::getConfigValue('cb_integration');
 		if ($row->user_id && $integration == 1)
 		{
 			$db = JFactory::getDBO();
@@ -40,8 +41,8 @@ class plgEventBookingCB extends JPlugin
 			if ($count)
 			{
 				return;
-			}
-			
+			}	
+
 			$sql = ' SHOW FIELDS FROM #__comprofiler ';
 			$db->setQuery($sql);
 			$fields = $db->loadObjectList();
@@ -51,32 +52,27 @@ class plgEventBookingCB extends JPlugin
 				$field = $fields[$i];
 				$fieldList[] = $field->Field;
 			}
-			$mappings = array(
-				'first_name' => $config->m_firstname, 
-				'last_name' => $config->m_lastname, 
-				'organization' => $config->m_organization, 
-				'address' => $config->m_address, 
-				'address2' => $config->m_address2, 
-				'city' => $config->m_city, 
-				'state' => $config->m_state, 
-				'zip' => $config->m_zip, 
-				'country' => $config->m_country, 
-				'phone' => $config->m_phone, 
-				'fax' => $config->m_fax);
 			
-			foreach ($mappings as $field => $cbField)
+			//Get list of fields belong to table		
+			$sql = 'SELECT name, field_mapping FROM #__eb_fields WHERE field_mapping != "" AND field_mapping IS NOT NULL AND is_core = 1';
+			$db->setQuery($sql);
+			$fields = $db->loadObjectList();
+			$fieldValues = array();
+			if (count($fields))
 			{
-				if ($cbField && in_array($cbField, $fieldList))
+				foreach ($fields as $field)
 				{
-					$fieldValues[$cbField] = $row->{$field};
+					$fieldName = $field->field_mapping;
+					if ($fieldName && in_array($fieldName, $fieldList))
+					{
+						$fieldValues[$fieldName] = $row->{$field->name};
+					}
 				}
-			}			
-			$sql = 'SELECT a.field_mapping, b.field_value FROM #__eb_fields AS a '
-			.' INNER JOIN #__eb_field_values AS b '
-			.' ON a.id = b.field_id '
-			.' WHERE b.registrant_id='.$row->id
+			}
 			
-			;			
+			$sql = 'SELECT a.field_mapping, b.field_value FROM #__eb_fields AS a ' . ' INNER JOIN #__eb_field_values AS b ' .
+				 ' ON a.id = b.field_id ' . ' WHERE b.registrant_id=' . $row->id;
+			
 			$db->setQuery($sql);
 			$fields = $db->loadObjectList();
 			if (count($fields))
@@ -85,11 +81,18 @@ class plgEventBookingCB extends JPlugin
 				{
 					if ($field->field_mapping && in_array($field->field_mapping, $fieldList))
 					{
-						$fieldValues[$field->field_mapping] = $field->field_value;
+						//Check if this is a json value
+						if (is_string($field->field_value) && is_array(json_decode($field->field_value)))
+						{
+							$fieldValues[$field->field_mapping] = implode('|*|', json_decode($field->field_value));
+						}
+						else
+						{
+							$fieldValues[$field->field_mapping] = $field->field_value;
+						}
 					}
 				}
 			}
-			
 			$profile = new stdClass();
 			$profile->id = $row->user_id;
 			$profile->user_id = $row->user_id;
@@ -104,8 +107,11 @@ class plgEventBookingCB extends JPlugin
 			{
 				$profile->{$fieldName} = $value;
 			}
-			$db->insertObject('#__comprofiler', $profile);
-			
+			$db->insertObject('#__comprofiler', $profile);			
+			//Update the block field in users table
+			$sql = 'UPDATE  #__users SET `block` = 0 WHERE id=' . $row->user_id;
+			$db->setQuery($sql);
+			$db->query();			
 			return true;
 		}
 	}
