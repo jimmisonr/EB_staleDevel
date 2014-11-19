512d89e93594be7f5626e15fb03cf6cbb8284514
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2014-11-19 11:48:32 +0700

    Fix Joomla groups plugin when cart is enabled
    Before, when cart is enabled, users will only be assigned to the Joomla
    groups which is setup for the first events, not other events in the
    cart. This PR simply fix that.

--- plugins/eventbooking/joomlagroups/joomlagroups.php
+++ plugins/eventbooking/joomlagroups/joomlagroups.php
@@ -11,55 +11,86 @@
 defined( '_JEXEC' ) or die ;
 
 class plgEventbookingJoomlagroups extends JPlugin
-{	
+{
 	public function __construct(& $subject, $config)
 	{
 		parent::__construct($subject, $config);
-		JFactory::getLanguage()->load('plg_eventbooking_joomlagroups', JPATH_ADMINISTRATOR);			
-		JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_eventbooking/tables');
+		JFactory::getLanguage()->load('plg_eventbooking_joomlagroups', JPATH_ADMINISTRATOR);
+		JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_eventbooking/tables');
 	}
+
 	/**
 	 * Render settings from
+	 *
 	 * @param Event $row
 	 */
-	function onEditEvent($row) {	
+	function onEditEvent($row)
+	{
 		ob_start();
-			$this->_drawSettingForm($row);		
-			$form = ob_get_contents();	
-		ob_end_clean();		
-		return array('title' => JText::_('PLG_EVENTBOOKING_JOOMLA_GROUPS_SETTINGS'),							
-					'form' => $form
-		) ;				
+		$this->_drawSettingForm($row);
+		$form = ob_get_contents();
+		ob_end_clean();
+
+		return array('title' => JText::_('PLG_EVENTBOOKING_JOOMLA_GROUPS_SETTINGS'),
+		             'form'  => $form
+		);
 	}
 
 	/**
 	 * Store setting into database
-	 * @param Event $row
+	 *
+	 * @param Event   $row
 	 * @param Boolean $isNew true if create new plan, false if edit
 	 */
-	function onAfterSaveEvent($row, $data,$isNew) {
-		$params = new JRegistry($row->params);		
-		$params->set('joomla_group_ids'			, implode(',',$data['joomla_group_ids']));
+	function onAfterSaveEvent($row, $data, $isNew)
+	{
+		$params = new JRegistry($row->params);
+		$params->set('joomla_group_ids', implode(',', $data['joomla_group_ids']));
 		$row->params = $params->toString();
-		
+
 		$row->store();
 	}
+
 	/**
 	 * Run when a membership activated
+	 *
 	 * @param PlanOsMembership $row
-	 */		
-	function onAfterPaymentSuccess($row) {		
-		if ($row->user_id) {
-			$user = JFactory::getUser($row->user_id);
-			$currentGroups  = $user->get('groups') ;			
-			$event = JTable::getInstance('EventBooking','Event');
-			$event->load($row->event_id);
-			$params = new JRegistry($event->params);
-			$groups = explode(',', $params->get('joomla_group_ids'));
-			$currentGroups = array_unique(array_merge($currentGroups, $groups)) ;
+	 */
+	function onAfterPaymentSuccess($row)
+	{
+		if ($row->user_id)
+		{
+			$user          = JFactory::getUser($row->user_id);
+			$currentGroups = $user->get('groups');
+			$event         = JTable::getInstance('EventBooking', 'Event');
+			$eventIds      = array($row->event_id);
+			$config        = EventbookingHelper::getConfig();
+			if ($config->multiple_booking)
+			{
+				// Get all events which users register for in this cart registration
+				$db    = JFactory::getDbo();
+				$query = $db->getQuery(true);
+				$query->select('event_id')
+					->from('#__eb_registrants')
+					->where('cart_id=' . $row->id);
+				$db->setQuery($query);
+				$eventIds = array_unique(array_merge($eventIds, $db->loadColumn()));
+			}
+			// Calculate the groups which registrant should be assigned to
+			foreach ($eventIds as $eventId)
+			{
+				$event->load($eventId);
+				$params   = new JRegistry($event->params);
+				$groupIds = $params->get('joomla_group_ids');
+				if ($groupIds)
+				{
+					$groups        = explode(',', $groupIds);
+					$currentGroups = array_unique(array_merge($currentGroups, $groups));
+				}
+			}
 			$user->set('groups', $currentGroups);
-			$user->save(true);					
-		}						
+			$user->save(true);
+		}
 	}
 	/**
 	 * Display form allows users to change setting for this subscription plan 
@@ -67,10 +98,8 @@ class plgEventbookingJoomlagroups extends JPlugin
 	 * 
 	 */	
 	function _drawSettingForm($row) {
-		// $row of table osmembership_plans
 		$params = new JRegistry($row->params);		
 		$joomla_group_ids 			= explode(',',$params->get('joomla_group_ids', ''));
-		$joomla_expried_group_ids 	= explode(',',$params->get('joomla_expried_group_ids', ''));		
 	?>	
 		<table class="admintable adminform" style="width: 90%;">
 				<tr>
@@ -79,11 +108,7 @@ class plgEventbookingJoomlagroups extends JPlugin
 					</td>
 					<td>
 						<?php
-							if (version_compare(JVERSION, '1.6.0', 'ge')) {
-								echo JHtml::_('access.usergroup', 'joomla_group_ids[]', $joomla_group_ids,  ' multiple="multiple" size="6" ', false) ;
-							} else {
-								
-							}
+							echo JHtml::_('access.usergroup', 'joomla_group_ids[]', $joomla_group_ids,  ' multiple="multiple" size="6" ', false) ;
 						?>
 					</td>
 					<td>
