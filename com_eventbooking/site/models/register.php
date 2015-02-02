a3f692599e352516643955e318518e585fc53740
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-02-02 10:57:47 +0700

    Tax amount error

--- com_eventbooking/site/models/register.php
+++ com_eventbooking/site/models/register.php
@@ -73,7 +73,7 @@ class EventBookingModelRegister extends JModelLegacy
 		}
 		$data['total_amount'] = round($fees['total_amount'], 2);
 		$data['discount_amount'] = round($fees['discount_amount'], 2);
-		$data['tax_amount'] = round($fees['tax_amount']);
+		$data['tax_amount'] = round($fees['tax_amount'], 2);
 		$data['amount'] = round($fees['amount'], 2);
 		$data['deposit_amount'] = $fees['deposit_amount'];
 		$data['payment_processing_fee'] = $fees['payment_processing_fee'];
