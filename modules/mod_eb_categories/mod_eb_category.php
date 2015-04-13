e8753eb777f8fa3fbfe3bbda72eb28cb733e3e39
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-04-13 21:18:05 +0700

    Multilingual for EB Categories Module

--- modules/mod_eb_categories/mod_eb_category.php
+++ modules/mod_eb_categories/mod_eb_category.php
@@ -1,37 +1,29 @@
 <?php
 /**
- * @version		1.6.1
- * @package		Joomla
- * @subpackage	Event Booking
- * @author  Tuan Pham Ngoc
- * @copyright	Copyright (C) 2010 Ossolution Team
- * @license		GNU/GPL, see LICENSE.php
+ * @version        1.7.2
+ * @package        Joomla
+ * @subpackage     Event Booking
+ * @author         Tuan Pham Ngoc
+ * @copyright      Copyright (C) 2010 Ossolution Team
+ * @license        GNU/GPL, see LICENSE.php
  */
-defined('_JEXEC') or die ;	
-$app = JFactory::getApplication() ;
-$db = JFactory::getDbo() ;
-$user = JFactory::getUser();
-require_once JPATH_ROOT.'/components/com_eventbooking/helper/helper.php';
-require_once JPATH_ROOT.'/components/com_eventbooking/helper/route.php';
-EventBookingHelper::loadLanguage();	
+defined('_JEXEC') or die ;
+$db = JFactory::getDbo();
+require_once JPATH_ROOT . '/components/com_eventbooking/helper/helper.php';
+require_once JPATH_ROOT . '/components/com_eventbooking/helper/route.php';
+EventBookingHelper::loadLanguage();
+$fieldSuffix      = EventbookingHelper::getFieldSuffix();
 $numberCategories = $params->get('number_categories', 0);
-if ($app->getLanguageFilter()) {
-	$extraWhere = ' AND a.language IN (' . $db->Quote(JFactory::getLanguage()->getTag()) . ',' . $db->Quote('*') . ')';
-} else {
-	$extraWhere = '' ;
-}
-
-$sql = 	'SELECT a.id, a.name, COUNT(b.id) AS total_categories  FROM #__eb_categories AS a LEFT JOIN #__eb_categories AS b ON (a.id = b.parent AND b.published =1) WHERE '
-.' a.parent = 0 AND a.published=1 AND a.access IN ('.implode(',', $user->getAuthorisedViewLevels()).')'
-.$extraWhere
-.' GROUP BY a.id '
-.' ORDER BY a.ordering '
-.( $numberCategories ? ' LIMIT '.$numberCategories : '')
-;
-      	
-$db->setQuery($sql) ;	
-$rows = $db->loadObjectList() ;
+$sql = 'SELECT a.id, a.name' . $fieldSuffix . ' AS name, COUNT(b.id) AS total_categories  FROM #__eb_categories AS a LEFT JOIN #__eb_categories AS b ON (a.id = b.parent AND b.published =1) WHERE '
+	. ' a.parent = 0 AND a.published=1 AND a.access IN (' . implode(',', JFactory::getUser()->getAuthorisedViewLevels()) . ')'
+	. ' GROUP BY a.id '
+	. ' ORDER BY a.ordering '
+	. ($numberCategories ? ' LIMIT ' . $numberCategories : '');
+$db->setQuery($sql);
+$rows   = $db->loadObjectList();
 $itemId = (int) $params->get('item_id');
-if (!$itemId)		
-    $itemId = EventBookingHelper::getItemid() ;			 
+if (!$itemId)
+{
+	$itemId = EventbookingHelper::getItemid();
+}
 require(JModuleHelper::getLayoutPath('mod_eb_category', 'default'));
--- modules/mod_eb_categories/tmpl/default.php
+++ modules/mod_eb_categories/tmpl/default.php
@@ -8,11 +8,13 @@
  * @license		GNU/GPL, see LICENSE.php
  */
 defined('_JEXEC') or die ( 'Restricted access');
-if (count($rows)) {		
+if (count($rows))
+{
 ?>
 	<ul class="menu">
 		<?php				
-			foreach ($rows  as $row) {
+			foreach ($rows  as $row)
+			{
 			?>
 				<li>
 					<a href="<?php echo JRoute::_(EventbookingHelperRoute::getCategoryRoute($row->id, $itemId)); ?>"><?php echo $row->name; ?></a>
