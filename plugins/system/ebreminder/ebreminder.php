b8ccb21ba2a5fea4463f0fed493e3a67b61a3dd5
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-06-29 09:54:24 +0700

    Improve EB Reminder Plugin to prevent multiple sending process happened at the same time

--- plugins/system/ebreminder/ebreminder.php
+++ plugins/system/ebreminder/ebreminder.php
@@ -1,48 +1,127 @@
 <?php
 /**
- * @version		1.5.0
- * @package		Joomla
- * @subpackage	Event Booking
- * @author  Tuan Pham Ngoc
- * @copyright	Copyright (C) 2010 Ossolution Team
- * @license		GNU/GPL, see LICENSE.php
+ * @version            1.7.4
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
+
 defined('_JEXEC') or die;
+
 class plgSystemEBReminder extends JPlugin
 {
-	function onAfterInitialise()
-	{		
+	public function onAfterRender()
+	{
 		error_reporting(0);
 		$secretCode = trim($this->params->get('secret_code'));
 		if ($secretCode && (JFactory::getApplication()->input->getString('secret_code') != $secretCode))
 		{
-			return ;
+			return;
 		}
-		if (file_exists(JPATH_ROOT.'/components/com_eventbooking/eventbooking.php'))
+		if (file_exists(JPATH_ROOT . '/components/com_eventbooking/eventbooking.php'))
 		{
-			$lastRun = (int) $this->params->get('last_run', 0);
+			$lastRun                 = (int) $this->params->get('last_run', 0);
 			$numberEmailSendEachTime = (int) $this->params->get('number_registrants', 0);
-			$currentTime = time() ;
-			$numberMinutes = ($currentTime - $lastRun)/60 ;
-			//This plugin win runs in each 10 minutes
-			if ($numberMinutes >= 30)
-			{
-				require_once JPATH_ROOT.'/components/com_eventbooking/helper/helper.php' ;
-				require_once JPATH_ROOT.'/components/com_eventbooking/models/reminder.php' ;
-				EventBookingModelReminder::sendReminder($numberEmailSendEachTime);
-				$db = JFactory::getDbo() ;
-				$query = $db->getQuery(true);
-				//Store last run time
-				$this->params->set('last_run', $currentTime);
-				$params = $this->params->toString();
+			$now                     = time();
+			$cacheTime               = 3600; // 60 minutes
 
-				$query->update('#__extensions')
-					->set('params='.$db->quote($params))
-					->where('`element`="ebreminder" AND `folder`="system"');
-				$db->setQuery($query);
-				$db->execute();
+			if (($now - $lastRun) < $cacheTime)
+			{
+				return;
 			}
-		}				
-		return true ;		
+
+			//Store last run time
+			$db    = JFactory::getDbo();
+			$query = $db->getQuery(true);
+			$this->params->set('last_run', $now);
+			$params = $this->params->toString();
+			$query->clear();
+			$query->update('#__extensions')
+				->set('params=' . $db->quote($params))
+				->where('`element`="ebreminder"')
+				->where('`folder`="system"');
+
+			try
+			{
+				// Lock the tables to prevent multiple plugin executions causing a race condition
+				$db->lockTable('#__extensions');
+			}
+			catch (Exception $e)
+			{
+				// If we can't lock the tables it's too risk continuing execution
+				return;
+			}
+
+			try
+			{
+				// Update the plugin parameters
+				$result = $db->setQuery($query)->execute();
+				$this->clearCacheGroups(array('com_plugins'), array(0, 1));
+			}
+			catch (Exception $exc)
+			{
+				// If we failed to execite
+				$db->unlockTables();
+				$result = false;
+			}
+			try
+			{
+				// Unlock the tables after writing
+				$db->unlockTables();
+			}
+			catch (Exception $e)
+			{
+				// If we can't lock the tables assume we have somehow failed
+				$result = false;
+			}
+			// Abort on failure
+			if (!$result)
+			{
+				return;
+			}
+
+			require_once JPATH_ROOT . '/components/com_eventbooking/helper/helper.php';
+			require_once JPATH_ROOT . '/components/com_eventbooking/models/reminder.php';
+			EventBookingModelReminder::sendReminder($numberEmailSendEachTime);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Clears cache groups. We use it to clear the plugins cache after we update the last run timestamp.
+	 *
+	 * @param   array $clearGroups  The cache groups to clean
+	 * @param   array $cacheClients The cache clients (site, admin) to clean
+	 *
+	 * @return  void
+	 *
+	 * @since   1.7.4
+	 */
+	private function clearCacheGroups(array $clearGroups, array $cacheClients = array(0, 1))
+	{
+		$conf = JFactory::getConfig();
+		foreach ($clearGroups as $group)
+		{
+			foreach ($cacheClients as $client_id)
+			{
+				try
+				{
+					$options = array(
+						'defaultgroup' => $group,
+						'cachebase'    => ($client_id) ? JPATH_ADMINISTRATOR . '/cache' :
+							$conf->get('cache_path', JPATH_SITE . '/cache')
+					);
+					$cache   = JCache::getInstance('callback', $options);
+					$cache->clean();
+				}
+				catch (Exception $e)
+				{
+					// Ignore it
+				}
+			}
+		}
 	}
 }
