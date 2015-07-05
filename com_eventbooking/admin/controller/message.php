10992c3e6644b0eb8c23fcc8da8ceb2a20fa3049
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-05 21:21:01 +0700

    Messages management conversion

--- com_eventbooking/admin/controller/message.php
+++ com_eventbooking/admin/controller/message.php
@@ -10,12 +10,6 @@
 // no direct access
 defined('_JEXEC') or die();
 
-/**
- * EventBooking Message controller
- *
- * @package		Joomla
- * @subpackage	Event Booking
- */
 class EventbookingControllerMessage extends EventbookingController
 {
 
@@ -24,11 +18,20 @@ class EventbookingControllerMessage extends EventbookingController
 		$data = $this->input->getData();
 		$model = $this->getModel();
 		$model->store($data);
-		$this->setRedirect('index.php?option=com_eventbooking&view=message', JText::_('EB_MESSAGES_SAVED'));
+
+		$task = $this->getTask();
+		if ($task == 'save')
+		{
+			$this->setRedirect('index.php?option=com_eventbooking&view=dashboard', JText::_('EB_MESSAGES_SAVED'));
+		}
+		else
+		{
+			$this->setRedirect('index.php?option=com_eventbooking&view=message', JText::_('EB_MESSAGES_SAVED'));
+		}
 	}
 	
 	public function cancel()
 	{
-		$this->setRedirect('index.php?option=com_eventbooking');
+		$this->setRedirect('index.php?option=com_eventbooking&view=dashboard');
 	}
 }
--- com_eventbooking/admin/view/message/html.php
+++ com_eventbooking/admin/view/message/html.php
@@ -1,34 +1,34 @@
 <?php
 /**
- * @version        	2.0.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2015 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die();
+defined('_JEXEC') or die;
+
 class EventbookingViewMessageHtml extends RADViewHtml
 {
 
-	function display()
-	{	
-		$languages = EventbookingHelper::getLanguages();	
-		$message = $this->model->getData();				
-		$this->message = $message;
+	public function display()
+	{
+		$languages       = EventbookingHelper::getLanguages();
+		$this->message   = EventbookingHelper::getMessages();
 		$this->languages = $languages;
 		$this->addToolbar();
-																															
+
 		parent::display();
 	}
-	
-	
-	public function addToolbar()
+
+
+	protected function addToolbar()
 	{
-		JToolBarHelper::title(   JText::_( 'Emails & Messages' ), 'generic.png' );
+		JToolBarHelper::title(JText::_('Emails & Messages'), 'generic.png');
+		JToolBarHelper::apply('apply', 'JTOOLBAR_APPLY');
 		JToolBarHelper::save('save');
 		JToolBarHelper::cancel('cancel');
-		$editor = JFactory::getEditor() ;
 	}
 }
