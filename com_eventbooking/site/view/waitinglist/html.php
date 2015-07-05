c33273aae8953723d72106ea41e73f0bbbfe7328
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-05 10:48:04 +0700

    Waitinglist view conversion

--- com_eventbooking/site/view/waitinglist/html.php
+++ com_eventbooking/site/view/waitinglist/html.php
@@ -1,50 +1,44 @@
 <?php
 /**
- * @version        	1.7.2
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2015 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 // no direct access
 defined('_JEXEC') or die();
-class EventBookingViewWaitinglist extends JViewLegacy
+
+class EventbookingViewWaitinglistHtml extends RADViewHtml
 {
 
 	/**
 	 * Display interface to user
 	 *
-	 * @param string $tpl
+	 * @throws Exception
 	 */
-	function display($tpl = null)
+	public function display()
 	{
-		$db               = JFactory::getDbo();
-		$query            = $db->getQuery(true);
-		$config           = EventbookingHelper::getConfig();
+		$db     = JFactory::getDbo();
+		$query  = $db->getQuery(true);
+		$config = EventbookingHelper::getConfig();
+
 		$registrationCode = JFactory::getSession()->get('eb_registration_code', '');
-		if ($registrationCode)
-		{
-			$sql = 'SELECT id FROM #__eb_registrants WHERE registration_code="' . $registrationCode . '" ORDER BY id LIMIT 1 ';
-			$db->setQuery($sql);
-			$id = (int) $db->loadResult();
-		}
-		else
-		{
-			JFactory::getApplication()->redirect('index.php', JText::_('EB_INVALID_REGISTRATION_CODE'));
-		}
-		if (!$id)
-		{
-			JFactory::getApplication()->redirect('index.php', JText::_('EB_INVALID_REGISTRATION_CODE'));
-		}
-		$query->select('a.*, b.payment_method')
-			->from('#__eb_events  AS a ')
-			->innerJoin('#__eb_registrants AS b ON a.id = b.event_id')
-			->where('b.id=' . $id);
+		$query->select('*')
+			->from('#__eb_registrants')
+			->where('registration_code = ' . $db->quote($registrationCode));
 		$db->setQuery($query);
-		$rowEvent    = $db->loadObject();
+		$rowRegistrant = $db->loadObject();
+		if (!$rowRegistrant)
+		{
+			JFactory::getApplication()->redirect('index.php', JText::_('EB_INVALID_REGISTRATION_CODE'));
+		}
+
+		$rowEvent    = EventbookingHelperDatabase::getEvent($rowRegistrant->event_id);
 		$message     = EventbookingHelper::getMessages();
 		$fieldSuffix = EventbookingHelper::getFieldSuffix();
+
 		if (strlen(strip_tags($message->{'waitinglist_complete_message' . $fieldSuffix})))
 		{
 			$msg = $message->{'waitinglist_complete_message' . $fieldSuffix};
@@ -54,12 +48,6 @@ class EventBookingViewWaitinglist extends JViewLegacy
 			$msg = $message->waitinglist_complete_message;
 		}
 
-		$query->clear();
-		$query->select('*')
-			->from('#__eb_registrants')
-			->where('id=' . $id);
-		$db->setQuery($query);
-		$rowRegistrant = $db->loadObject();
 		if (EventbookingHelper::isGroupRegistration($rowRegistrant->id))
 		{
 			$rowFields = EventbookingHelper::getFormFields($rowEvent->id, 1);
@@ -75,11 +63,11 @@ class EventBookingViewWaitinglist extends JViewLegacy
 		$replaces = EventbookingHelper::buildTags($rowRegistrant, $form, $rowEvent, $config);
 		foreach ($replaces as $key => $value)
 		{
-			$key          = strtoupper($key);
+			$key = strtoupper($key);
 			$msg = str_replace("[$key]", $value, $msg);
 		}
-		$this->message          = $msg;
+		$this->message = $msg;
 
-		parent::display($tpl);
+		parent::display();
 	}
 }
