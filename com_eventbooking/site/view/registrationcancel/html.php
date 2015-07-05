ec65f8b9119cae7b54d98f3bc0050162435ebfed
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-05 11:13:33 +0700

    Registration cancel view conversion

--- com_eventbooking/site/view/registrationcancel/html.php
+++ com_eventbooking/site/view/registrationcancel/html.php
@@ -1,35 +1,41 @@
 <?php
 /**
- * @version        	2.0.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2015 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 // no direct access
 defined('_JEXEC') or die();
-class EventBookingViewRegistrationCancel extends JViewLegacy
-{
 
-	function display($tpl = null)
+class EventbookingViewRegistrationcancelHtml extends RADViewHtml
+{
+	public $hasModel = false;
+
+	public function display()
 	{
 		$this->setLayout('default');
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		$id = JRequest::getInt('id', 0);
-		if (!$id)
-		{
-			return;
-		}
-		$message = EventbookingHelper::getMessages();
+
+		$db          = JFactory::getDbo();
+		$query       = $db->getQuery(true);
+		$message     = EventbookingHelper::getMessages();
 		$fieldSuffix = EventbookingHelper::getFieldSuffix();
-		$query->select('*')
-			->from('#__eb_registrants')
-			->where('id=' . $id);
+		$id          = $this->input->getInt('id', 0);
+		$query->select('a.*, b.title' . $fieldSuffix . ' AS event_title')
+			->from('#__eb_registrants AS a')
+			->innerJoin('#__eb_events AS b ON a.event_id = b.id')
+			->where('a.id=' . $id);
 		$db->setQuery($query);
-		$row = $db->loadObject();
-		if ($row->amount > 0)
+		$rowRegistrant = $db->loadObject();
+
+		if (!$rowRegistrant)
+		{
+			JFactory::getApplication()->redirect('index.php', JText::_('EB_INVALID_REGISTRATION_CODE'));
+		}
+
+		if ($rowRegistrant->amount > 0)
 		{
 			if (strlen(trim(strip_tags($message->{'registration_cancel_message_paid' . $fieldSuffix}))))
 			{
@@ -51,15 +57,10 @@ class EventBookingViewRegistrationCancel extends JViewLegacy
 				$cancelMessage = $message->registration_cancel_message_free;
 			}
 		}
-		$query->clear();
-		$query->select('a.title'.$fieldSuffix)
-			->from('#__eb_events AS a')
-			->innerJoin('#__eb_registrants AS b ON a.id = b.event_id')
-			->where('b.id=' . $id);
-		$db->setQuery($query);
-		$title = $db->loadResult();
-		$cancelMessage = str_replace('[EVENT_TITLE]', $title, $cancelMessage);
+
+		$cancelMessage = str_replace('[EVENT_TITLE]', $rowRegistrant->event_title, $cancelMessage);
 		$this->message = $cancelMessage;
-		parent::display($tpl);
+
+		parent::display();
 	}
 }
