8a4675e9c1e2c8266e58786a6454cf6660cbde70
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-05 19:14:37 +0700

    Map view conversion

--- com_eventbooking/site/view/map/html.php
+++ com_eventbooking/site/view/map/html.php
@@ -1,28 +1,27 @@
 <?php
 /**
- * @version        	2.0.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2015 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 // no direct access
 defined('_JEXEC') or die();
-class EventBookingViewMap extends JViewLegacy
-{
 
-	function display($tpl = null)
+class EventbookingViewMapHtml extends RADViewHtml
+{
+	public $hasModel = false;
+
+	public function display()
 	{
 		$this->setLayout('default');
-		$db = JFactory::getDbo();
-		$config = EventbookingHelper::getConfig();
-		$locationId = JRequest::getInt('location_id', 0);
-		$sql = 'SELECT * FROM #__eb_locations WHERE id=' . $locationId;
-		$db->setQuery($sql);
-		$location = $db->loadObject();
-		$this->assignRef('location', $location);
-		$this->assignRef('config', $config);
-		parent::display($tpl);
+		$locationId     = $this->input->getInt('location_id', 0);
+		$location       = EventbookingHelperDatabase::getLocation($locationId);
+		$this->location = $location;
+		$this->config   = EventbookingHelper::getConfig();
+
+		parent::display();
 	}
 }
