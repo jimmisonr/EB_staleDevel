5a40f6e3a2587bb535648f2dbeaa18a35dbf2b67
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-06 14:10:07 +0700

    Languages implement

--- com_eventbooking/admin/controller/language.php
+++ com_eventbooking/admin/controller/language.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @version        	2.0.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2015 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 // no direct access
 defined('_JEXEC') or die();
@@ -13,24 +13,34 @@ defined('_JEXEC') or die();
 /**
  * EventBooking Language controller
  *
- * @package		Joomla
- * @subpackage	Event Booking
+ * @package        Joomla
+ * @subpackage     Event Booking
  */
 class EventbookingControllerLanguage extends EventbookingController
 {
-	public function save() {
-		$data = $this->input->getData();		
-		$model = $this->getModel() ;
+	public function save()
+	{
+		$data  = $this->input->getData();
+		$model = $this->getModel();
 		$model->save($data);
-		$this->setRedirect('index.php?option=com_eventbooking&view=language');
+
+		$task = $this->getTask();
+		if ($task == 'apply')
+		{
+			$this->setRedirect('index.php?option=com_eventbooking&view=language');
+		}
+		else
+		{
+			$this->setRedirect('index.php?option=com_eventbooking&view=language&view=dashboard');
+		}
 	}
 
 	/**
 	 * Cancel registration, redirect to dashboard page
-	 * 	 
+	 *
 	 */
 	public function cancel()
-	{				
-		$this->setRedirect('index.php?option=com_eventbooking');		
+	{
+		$this->setRedirect('index.php?option=com_eventbooking&view=dashboard');
 	}
 }
