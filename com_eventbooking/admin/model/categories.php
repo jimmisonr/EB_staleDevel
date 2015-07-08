8bf222b50e4206cdae4f493b4188e64067c831ef
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-08 15:28:50 +0700

    Categories view code review

--- com_eventbooking/admin/model/categories.php
+++ com_eventbooking/admin/model/categories.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * @version        	2.0.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2015 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 // no direct access
 defined('_JEXEC') or die();
+
 class EventbookingModelCategories extends RADModelList
 {
 
 	public function __construct($config = array())
-	{				
+	{
 		parent::__construct($config);
-				
+
 		$this->state->insert('filter_parent', 'int', 0);
 	}
 
@@ -30,38 +31,38 @@ class EventbookingModelCategories extends RADModelList
 		// Lets load the content if it doesn't already exist
 		if (empty($this->data))
 		{
-			$parent = $this->state->filter_parent;			
-			$db = $this->getDbo();
-			$query = $db->getQuery(true);
+			$parent = $this->state->filter_parent;
+			$db     = $this->getDbo();
+			$query  = $db->getQuery(true);
 			$this->buildQueryColumns($query)
 				->buildQueryFrom($query)
-				->_buildQueryJoins($query)
-				->_buildQueryWhere($query)
-				->_buildQueryGroup($query)
-				->_buildQueryOrder($query);								
+				->buildQueryJoins($query)
+				->buildQueryWhere($query)
+				->buildQueryGroup($query)
+				->buildQueryOrder($query);
 			$db->setQuery($query);
-			$rows = $db->loadObjectList();			
+			$rows     = $db->loadObjectList();
 			$children = array();
 			// first pass - collect children
 			if (count($rows))
 			{
 				foreach ($rows as $v)
 				{
-					$pt = $v->parent;
+					$pt   = $v->parent;
 					$list = @$children[$pt] ? $children[$pt] : array();
 					array_push($list, $v);
 					$children[$pt] = $list;
 				}
 			}
-			$list = JHtml::_('menu.treerecurse', $parent, '', array(), $children, 9999);
+			$list  = JHtml::_('menu.treerecurse', $parent, '', array(), $children, 9999);
 			$total = count($list);
 			jimport('joomla.html.pagination');
 			$this->pagination = new JPagination($total, $this->state->limitstart, $this->state->limit);
 			// slice out elements based on limits
-			$list = array_slice($list, $this->pagination->limitstart, $this->pagination->limit);
+			$list       = array_slice($list, $this->pagination->limitstart, $this->pagination->limit);
 			$this->data = $list;
 		}
-		
+
 		return $this->data;
 	}
 
@@ -71,7 +72,7 @@ class EventbookingModelCategories extends RADModelList
 	protected function buildQueryColumns(JDatabaseQuery $query)
 	{
 		$query->select('tbl.*, tbl.parent AS parent_id, tbl.name AS title, COUNT(ec.id) AS total_events');
-		
+
 		return $this;
 	}
 
@@ -81,7 +82,7 @@ class EventbookingModelCategories extends RADModelList
 	protected function buildQueryJoins(JDatabaseQuery $query)
 	{
 		$query->leftJoin('#__eb_event_categories AS ec ON tbl.id = ec.category_id');
-		
+
 		return $this;
 	}
 
@@ -91,7 +92,7 @@ class EventbookingModelCategories extends RADModelList
 	protected function buildQueryGroup(JDatabaseQuery $query)
 	{
 		$query->group('tbl.id');
-		
+
 		return $this;
 	}
 }
