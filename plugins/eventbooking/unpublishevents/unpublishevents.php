193b963b11ea2b54611100358113232e710d60ab
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-13 11:12:19 +0700

    Unpublish events plugin code review

--- plugins/eventbooking/unpublishevents/unpublishevents.php
+++ plugins/eventbooking/unpublishevents/unpublishevents.php
@@ -1,48 +1,60 @@
 <?php
 /**
- * @version		1.5.0
- * @package		Joomla
- * @subpackage	Event Booking
- * @author  Tuan Pham Ngoc
- * @copyright	Copyright (C) 2010 Ossolution Team
- * @license		GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
-// No direct access.
+
+// no direct access
 defined('_JEXEC') or die;
-jimport('joomla.plugin.plugin');
+
 class plgEventBookingUnpublishEvents extends JPlugin
-{	
-	public function __construct(& $subject, $config)
-	{		
-		parent::__construct($subject, $config);		
-	}
-	
-	
-	function onAfterStoreRegistrant($row) {
-		if ($row->payment_method == 'os_offline') {
-			$this->_processUnpublishEvent($row->event_id) ;
+{
+	public function onAfterStoreRegistrant($row)
+	{
+		if (strpos($row->payment_method, 'os_offline') !== false)
+		{
+			$this->processUnpublishEvent($row->event_id);
 		}
 	}
-	
-	public function onAfterPaymentSuccess($row) {
-		$this->_processUnpublishEvent($row->event_id) ;		
-	} 
-		
-	function _processUnpublishEvent($eventId) {
-		$db = & JFactory::getDbo() ;
-		$sql = 'SELECT event_capacity FROM #__eb_events WHERE id='.$eventId;
-		$db->setQuery($sql);		
-		$capacity = (int) $db->loadResult();		
-		if ($capacity > 0) {
-			$sql = 'SELECT COUNT(*) FROM #__eb_registrants AS b WHERE event_id='.$eventId.' AND b.group_id=0 AND (b.published = 1 OR (b.payment_method LIKE "os_offline%" AND b.published != 2))';
-			$db->setQuery($sql);
-			$totalRegistrants = (int) $db->loadResult() ;			
-			if ($totalRegistrants >= $capacity) {								
-				//Unpublish the event
-				$sql = 'UPDATE #__eb_events SET published=0 WHERE id='.$eventId;
-				$db->setQuery($sql) ;
-				$db->query();
+
+	public function onAfterPaymentSuccess($row)
+	{
+		$this->processUnpublishEvent($row->event_id);
+	}
+
+	private function processUnpublishEvent($eventId)
+	{
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('event_capacity')
+			->from('#__eb_events')
+			->where('id = ' . $eventId);
+		$db->setQuery($query);
+		$capacity = (int) $db->loadResult();
+		if ($capacity > 0)
+		{
+			$query->clear();
+			$query->select('COUNT(*)')
+				->from('#__eb_registrants AS b')
+				->where('event_id = ' . (int) $eventId)
+				->where('b.group_id = 0')
+				->where('(b.published = 1 OR (b.payment_method LIKE "os_offline%" AND b.published NOT IN (2,3)))');
+			$db->setQuery($query);
+			$totalRegistrants = (int) $db->loadResult();
+			if ($totalRegistrants >= $capacity)
+			{
+				// Un-publish the event
+				$query->clear();
+				$query->update('#__eb_events')
+					->set('published = 0')
+					->where('id = ' . (int) $eventId);
+				$db->setQuery($query);
+				$db->execute();
 			}
-		}	
+		}
 	}
 }
