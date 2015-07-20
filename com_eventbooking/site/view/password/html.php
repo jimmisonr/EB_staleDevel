664ca2155464fd9db6b818180c68b1f5651f8e4b
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-07-20 08:59:06 +0700

    Fix event password error

--- com_eventbooking/site/view/password/html.php
+++ com_eventbooking/site/view/password/html.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * @version        	2.0.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2015 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @version            2.0.0
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 // no direct access
-defined('_JEXEC') or die();
-class EventBookingViewPassword extends JViewLegacy
+defined('_JEXEC') or die;
+
+class EventbookingViewPasswordHtml extends RADViewHtml
 {
 
-	function display($tpl = null)
+	public function display()
 	{
 		$this->setLayout('default');
-		$this->Itemid = JRequest::getInt('Itemid', 0);
-		$this->return = JRequest::getVar('return', '', 'none');
-		$this->eventId = JRequest::getInt('event_id');
-		$this->eventUrl = EventbookingHelperRoute::getEventRoute($this->eventId, 0, $this->Itemid);
-		$config = EventbookingHelper::getConfig();
+		$this->return          = $this->input->get('return', '', 'none');
+		$this->eventId         = $this->input->getInt('event_id', 0);
+		$this->eventUrl        = EventbookingHelperRoute::getEventRoute($this->eventId, 0, $this->Itemid);
+		$config                = EventbookingHelper::getConfig();
 		$this->bootstrapHelper = new EventbookingHelperBootstrap($config->twitter_bootstrap_version);
 
-		parent::display($tpl);
+		parent::display();
 	}
 }
