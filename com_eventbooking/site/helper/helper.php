0f5def2e409771482236c1546ef7e186bcb8957c
Tuan Pham Ngoc <github@joomdonation.com>
2015-12-09 21:50:33 +0700

    Update version number to 2.2.0

--- com_eventbooking/site/helper/helper.php
+++ com_eventbooking/site/helper/helper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version            2.1.0
+ * @version            2.2.0
  * @package            Joomla
  * @subpackage         Event Booking
  * @author             Tuan Pham Ngoc
@@ -18,7 +18,7 @@ class EventbookingHelper
 	 */
 	public static function getInstalledVersion()
 	{
-		return '2.1.0';
+		return '2.2.0';
 	}
 
 	/**
