c6b508911b8c9503c6ab8370f345f5af54fe4850
Tuan Pham Ngoc <github@joomdonation.com>
2016-01-02 14:24:18 +0700

    Update copyright to 2016

--- com_eventbooking/site/helper/database.php
+++ com_eventbooking/site/helper/database.php
@@ -5,7 +5,7 @@
  * @package            Joomla
  * @subpackage         Event Booking
  * @author             Tuan Pham Ngoc
- * @copyright          Copyright (C) 2010 - 2015 Ossolution Team
+ * @copyright          Copyright (C) 2010 - 2016 Ossolution Team
  * @license            GNU/GPL, see LICENSE.php
  */
 class EventbookingHelperDatabase
