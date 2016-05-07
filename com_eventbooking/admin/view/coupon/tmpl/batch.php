f39bef2975dcbe06d2e94d51ee7a1da80f842a9e
Tuan Pham Ngoc <github@joomdonation.com>
2016-05-07 11:03:30 +0700

    Convert coupon form to tableless layout

--- com_eventbooking/admin/view/coupon/tmpl/batch.php
+++ com_eventbooking/admin/view/coupon/tmpl/batch.php
@@ -8,99 +8,96 @@
  * @license            GNU/GPL, see LICENSE.php
  */
 // no direct access
-defined( '_JEXEC' ) or die ; 
+defined( '_JEXEC' ) or die ;
+JHtml::_('formbehavior.chosen', 'select');
+
 JToolBarHelper::title(JText::_('EB_BATCH_COUPONS_TITLE'));
 JToolBarHelper::custom('coupon.batch', 'upload', 'upload', 'Generate Coupons', false);
 JToolBarHelper::cancel('coupon.cancel');	
 ?>
-<form action="index.php?option=com_eventbooking&view=coupon" method="post" name="adminForm" id="adminForm">
-<div class="row-fluid">
-			<table class="admintable adminform">
-				<tr>
-					<td width="200" class="key">
-						<?php echo  JText::_('EB_NUMBER_COUPONS'); ?>
-					</td>
-					<td>
-						<input class="input-mini" type="text" name="number_coupon" id="number_coupon" size="15" maxlength="250" value="" />
-					</td>
-				</tr>
-				<tr>
-					<td class="key">
-						<?php echo JText::_('EB_EVENTS'); ?>
-					</td>
-					<td>
-						<?php echo $this->lists['event_id']; ?>
-					</td>
-				</tr>
-				<tr>
-					<td width="100" class="key">
-						<?php echo  JText::_('EB_DISCOUNT'); ?>
-					</td>
-					<td>
-						<input class="text_area" type="text" name="discount" id="discount" size="10" maxlength="250" value="" />&nbsp;&nbsp;<?php echo $this->lists['coupon_type'] ; ?>
-					</td>
-				</tr>
-				<tr>
-					<td width="100" class="key">
-						<?php echo  JText::_('EB_CHARACTERS_SET'); ?>
-					</td>
-					<td>
-						<input class="text_area" type="text" name="characters_set" id="characters_set" size="15" maxlength="250" value="" />
-					</td>
-				</tr>
-				<tr>
-					<td width="100" class="key">
-						<?php echo  JText::_('EB_PREFIX'); ?>
-					</td>
-					<td>
-						<input class="text_area" type="text" name="prefix" id="prefix" size="15" maxlength="250" value="" />
-					</td>
-				</tr>
-				<tr>
-					<td width="100" class="key">
-						<?php echo  JText::_('EB_COUPON_LENGTH'); ?>
-					</td>
-					<td>
-						<input class="text_area" type="text" name="length" id="length" size="15" maxlength="250" value="" />
-					</td>
-				</tr>
-				<tr>
-					<td class="key">
-						<?php echo JText::_('EB_VALID_FROM_DATE'); ?>
-					</td>
-					<td>
-						<?php echo JHtml::_('calendar', '', 'valid_from', 'valid_from') ; ?>
-					</td>
-				</tr>
-				<tr>
-					<td class="key">
-						<?php echo JText::_('EB_VALID_TO_DATE'); ?>
-					</td>
-					<td>
-						<?php echo JHtml::_('calendar', '', 'valid_to', 'valid_to') ; ?>
-					</td>
-				</tr>
-				<tr>
-					<td class="key">
-						<?php echo JText::_('EB_TIMES'); ?>
-					</td>
-					<td>
-						<input class="text_area" type="text" name="times" id="times" size="5" maxlength="250" value="<?php echo $this->item->times;?>" />
-					</td>
-				</tr>	
-				<tr>
-					<td class="key">
-						<?php echo JText::_('EB_PUBLISHED'); ?>
-					</td>
-					<td>
-						<?php echo $this->lists['published']; ?>
-					</td>
-				</tr>
-		</table>							
-</div>		
-<div class="clr"></div>	
+<form action="index.php?option=com_eventbooking&view=coupon" method="post" name="adminForm" id="adminForm" class="form form-horizontal">		
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo  JText::_('EB_NUMBER_COUPONS'); ?>
+		</label>
+		<div class="controls">
+			<input class="input-mini" type="text" name="number_coupon" id="number_coupon" size="15" maxlength="250" value="" />
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo JText::_('EB_EVENTS'); ?>
+		</label>
+		<div class="controls">
+			<?php echo $this->lists['event_id']; ?>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo  JText::_('EB_DISCOUNT'); ?>
+		</label>
+		<div class="controls">
+			<input class="text_area" type="text" name="discount" id="discount" size="10" maxlength="250" value="" />&nbsp;&nbsp;<?php echo $this->lists['coupon_type'] ; ?>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo  JText::_('EB_CHARACTERS_SET'); ?>
+		</label>
+		<div class="controls">
+			<input class="text_area" type="text" name="characters_set" id="characters_set" size="15" maxlength="250" value="" />
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo  JText::_('EB_PREFIX'); ?>
+		</label>
+		<div class="controls">
+			<input class="text_area" type="text" name="prefix" id="prefix" size="15" maxlength="250" value="" />
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo  JText::_('EB_COUPON_LENGTH'); ?>
+		</label>
+		<div class="controls">
+			<input class="text_area" type="text" name="length" id="length" size="15" maxlength="250" value="" />
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo JText::_('EB_VALID_FROM_DATE'); ?>
+		</label>
+		<div class="controls">
+			<?php echo JHtml::_('calendar', '', 'valid_from', 'valid_from') ; ?>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo JText::_('EB_VALID_TO_DATE'); ?>
+		</label>
+		<div class="controls">
+			<?php echo JHtml::_('calendar', '', 'valid_to', 'valid_to') ; ?>
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo JText::_('EB_TIMES'); ?>
+		</label>
+		<div class="controls">
+			<input class="text_area" type="text" name="times" id="times" size="5" maxlength="250" value="" />
+		</div>
+	</div>	
+	<div class="control-group">
+		<label class="control-label">
+			<?php echo JText::_('EB_PUBLISHED'); ?>
+		</label>
+		<div class="controls">
+			<?php echo $this->lists['published']; ?>
+		</div>
+	</div>
+	<div class="clearfix"></div>
 	<?php echo JHtml::_( 'form.token' ); ?>
-	<input type="hidden" name="used" value="<?php echo $this->item->used;?>" />
-	<input type="hidden" name="id" value="<?php echo $this->item->id; ?>" />
+	<input type="hidden" name="used" value="0"/>
 	<input type="hidden" name="task" value="" />
 </form>
