93c29a77844ef29cfbf3d6398b3e8feb0262dcfc
Tuan Pham Ngoc <github@joomdonation.com>
2016-05-08 15:01:10 +0700

    Add deposit payment view

---
+++ com_eventbooking/site/view/payment/html.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * @version            2.4.3
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2016 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
+ */
+// no direct access
+defined('_JEXEC') or die;
+
+class EventbookingViewPaymentHtml extends RADViewHtml
+{
+	/**
+	 * Display interface to user
+	 *
+	 */
+	public function display()
+	{
+		// Load common js code
+		$document = JFactory::getDocument();
+		$document->addScriptDeclaration(
+			'var siteUrl = "' . EventbookingHelper::getSiteUrl() . '";'
+		);
+		$document->addScript(JUri::root(true) . '/media/com_eventbooking/assets/js/paymentmethods.js');
+		EventbookingHelper::addLangLinkForAjax();
+
+		$config                = EventbookingHelper::getConfig();
+		$this->bootstrapHelper = new EventbookingHelperBootstrap($config->twitter_bootstrap_version);
+
+		$input   = $this->input;
+		$registrantId = $input->getInt('registrant_id');
+
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('*')
+			->from('#__eb_registrants')
+			->where('id = '. $registrantId);
+		$db->setQuery($query);
+		$rowRegistrant = $db->loadObject();
+		if (empty($rowRegistrant))
+		{
+			echo JText::_('EB_INVALID_REGISTRATION_RECORD');
+
+			return;
+		}
+
+		if ($rowRegistrant->payment_status == 1)
+		{
+			echo JText::_('EB_DEPOSIT_PAYMENT_COMPLETED');
+
+			return;
+		}
+
+		$event   = EventbookingHelperDatabase::getEvent($rowRegistrant->event_id);
+
+		$config    = EventbookingHelper::getConfig();
+		$user      = JFactory::getUser();
+		$userId    = $user->get('id');
+		$rowFields = EventbookingHelper::getDepositPaymentFormFields();
+
+		$captchaInvalid = $input->getInt('captcha_invalid', 0);
+		if ($captchaInvalid)
+		{
+			$data = $input->post->getData();
+		}
+		else
+		{
+			$data = EventbookingHelper::getRegistrantData($rowRegistrant, $rowFields);
+
+			// IN case there is no data, get it from URL (get for example)
+			if (empty($data))
+			{
+				$data = $input->getData();
+			}
+		}
+
+		if ($userId && !isset($data['first_name']))
+		{
+			//Load the name from Joomla default name
+			$name = $user->name;
+			if ($name)
+			{
+				$pos = strpos($name, ' ');
+				if ($pos !== false)
+				{
+					$data['first_name'] = substr($name, 0, $pos);
+					$data['last_name']  = substr($name, $pos + 1);
+				}
+				else
+				{
+					$data['first_name'] = $name;
+					$data['last_name']  = '';
+				}
+			}
+		}
+		if ($userId && !isset($data['email']))
+		{
+			$data['email'] = $user->email;
+		}
+		if (!isset($data['country']) || !$data['country'])
+		{
+			$data['country'] = $config->default_country;
+		}
+
+		//Get data
+		$form = new RADForm($rowFields);
+		if ($captchaInvalid)
+		{
+			$useDefault = false;
+		}
+		else
+		{
+			$useDefault = true;
+		}
+		$form->bind($data, $useDefault);
+
+		$paymentMethod = $input->post->getString('payment_method', os_payments::getDefautPaymentMethod(trim($event->payment_methods)));
+
+		$expMonth            = $input->post->getInt('exp_month', date('m'));
+		$expYear             = $input->post->getInt('exp_year', date('Y'));
+		$lists['exp_month']  = JHtml::_('select.integerlist', 1, 12, 1, 'exp_month', ' class="input-small" ', $expMonth, '%02d');
+		$currentYear         = date('Y');
+		$lists['exp_year']   = JHtml::_('select.integerlist', $currentYear, $currentYear + 10, 1, 'exp_year', 'class="input-small"', $expYear);
+
+		$methods            = os_payments::getPaymentMethods(trim($event->payment_methods));
+
+
+		$this->loadCaptcha();
+
+		// Assign these parameters
+		$this->paymentMethod        = $paymentMethod;
+		$this->config               = $config;
+		$this->event                = $event;
+		$this->methods              = $methods;
+		$this->lists                = $lists;
+		$this->message              = EventbookingHelper::getMessages();
+		$this->fieldSuffix          = EventbookingHelper::getFieldSuffix();
+		$this->message              = EventbookingHelper::getMessages();
+		$this->form                 = $form;
+		$this->rowRegistrant        = $rowRegistrant;
+
+		parent::display();
+	}
+	/**
+	 * Load captcha for registration form
+	 * @param bool $initOnly
+	 *
+	 * @throws Exception
+	 */
+	private function loadCaptcha($initOnly = false)
+	{
+		$config = EventbookingHelper::getConfig();
+		$user   = JFactory::getUser();
+		$showCaptcha = 0;
+
+		if ($config->enable_captcha && ($user->id == 0 || $config->bypass_captcha_for_registered_user !== '1'))
+		{
+			$captchaPlugin = JFactory::getApplication()->getParams()->get('captcha', JFactory::getConfig()->get('captcha'));
+			if (!$captchaPlugin)
+			{
+				// Hardcode to recaptcha, reduce support request
+				$captchaPlugin = 'recaptcha';
+			}
+			$plugin = JPluginHelper::getPlugin('captcha', $captchaPlugin);
+			if ($plugin)
+			{
+				$showCaptcha   = 1;
+				if ($initOnly)
+				{
+					JCaptcha::getInstance($captchaPlugin)->initialise('dynamic_recaptcha_1');
+				}
+				else
+				{
+					$this->captcha = JCaptcha::getInstance($captchaPlugin)->display('dynamic_recaptcha_1', 'dynamic_recaptcha_1', 'required');
+				}
+			}
+			else
+			{
+				JFactory::getApplication()->enqueueMessage(JText::_('EB_CAPTCHA_NOT_ACTIVATED_IN_YOUR_SITE'), 'error');
+			}
+		}
+
+		$this->showCaptcha = $showCaptcha;
+	}
+}
---
+++ com_eventbooking/site/view/payment/tmpl/default.php
@@ -0,0 +1,288 @@
+<?php
+/**
+ * @version            2.4.3
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2016 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
+ */
+// no direct access
+defined( '_JEXEC' ) or die ;
+EventbookingHelperJquery::validateForm();
+
+$headerText = JText::_('EB_DEPOSIT_PAYMENT');
+
+if (strlen(strip_tags($this->message->{'deposit_payment_form_message' . $this->fieldSuffix})))
+{
+	$msg = $this->message->{'deposit_payment_form_message' . $this->fieldSuffix};
+}
+else
+{
+
+	$msg = $this->message->deposit_payment_form_message;
+}
+
+$msg = str_replace('[AMOUNT]', EventbookingHelper::formatCurrency($this->rowRegistrant->amount = $this->rowRegistrant->deposit_amount, $this->config, $this->event->currency_symbol), $msg);
+$msg = str_replace('[REGISTRATION_ID]', $this->rowRegistrant->id, $msg);
+
+$replaces = EventbookingHelper::buildEventTags($this->event, $this->config);
+foreach ($replaces as $key => $value)
+{
+	foreach ($replaces as $key => $value)
+	{
+		$key        = strtoupper($key);
+		$msg        = str_replace("[$key]", $value, $msg);
+		$headerText = str_replace("[$key]", $value, $headerText);
+	}
+}
+
+if ($this->config->use_https)
+{
+	$url = JRoute::_('index.php?option=com_eventbooking&task=payment.process&Itemid='.$this->Itemid, false, 1);
+}
+else
+{
+	$url = JRoute::_('index.php?option=com_eventbooking&task=payment.process&Itemid='.$this->Itemid, false);
+}
+$selectedState = '';
+
+// Bootstrap classes
+$bootstrapHelper   = $this->bootstrapHelper;
+$controlGroupClass = $bootstrapHelper->getClassMapping('control-group');
+$controlLabelClass = $bootstrapHelper->getClassMapping('control-label');
+$controlsClass     = $bootstrapHelper->getClassMapping('controls');
+
+/* @var EventbookingViewRegisterHtml $this */
+?>
+<div id="eb-deposit-payment-page" class="eb-container">
+	<h1 class="eb-page-heading"><?php echo $headerText; ?></h1>
+	<form method="post" name="adminForm" id="adminForm" action="<?php echo $url; ?>" autocomplete="off" class="form form-horizontal" enctype="multipart/form-data">
+	<?php
+	if (strlen($msg))
+	{
+	?>
+		<div class="eb-message"><?php echo $msg; ?></div>
+	<?php
+	}
+	$fields = $this->form->getFields();
+	if (isset($fields['state']))
+	{
+		$selectedState = $fields['state']->value;
+	}
+	foreach ($fields as $field)
+	{
+		echo $field->getControlGroup($bootstrapHelper);
+	}
+
+		if (count($this->methods) > 1)
+		{
+			?>
+			<div class="<?php echo $controlGroupClass;  ?> payment_information" id="payment_method_container">
+				<label class="<?php echo $controlLabelClass; ?>" for="payment_method">
+					<?php echo JText::_('EB_PAYMENT_OPTION'); ?>
+					<span class="required">*</span>
+				</label>
+
+				<div class="<?php echo $controlsClass; ?>">
+					<?php
+					$method = null;
+					for ($i = 0, $n = count($this->methods); $i < $n; $i++)
+					{
+						$paymentMethod = $this->methods[$i];
+						if ($paymentMethod->getName() == $this->paymentMethod)
+						{
+							$checked = ' checked="checked" ';
+							$method  = $paymentMethod;
+						}
+						else
+						{
+							$checked = '';
+						}
+						?>
+						<label class="radio">
+							<input onclick="changePaymentMethod('individual');" class="validate[required] radio"
+								   type="radio" name="payment_method"
+								   value="<?php echo $paymentMethod->getName(); ?>" <?php echo $checked; ?> /><?php echo JText::_($paymentMethod->getTitle()); ?>
+						</label>
+					<?php
+					}
+					?>
+				</div>
+			</div>
+		<?php
+		}
+		else
+		{
+			$method = $this->methods[0];
+			?>
+			<div class="<?php echo $controlGroupClass;  ?> payment_information" id="payment_method_container">
+				<label class="<?php echo $controlLabelClass; ?>">
+					<?php echo JText::_('EB_PAYMENT_OPTION'); ?>
+				</label>
+
+				<div class="<?php echo $controlsClass; ?>">
+					<?php echo JText::_($method->getTitle()); ?>
+				</div>
+			</div>
+		<?php
+		}
+		if ($method->getCreditCard())
+		{
+			$style = '';
+		}
+		else
+		{
+			$style = 'style = "display:none"';
+		}
+		?>
+		<div class="<?php echo $controlGroupClass;  ?> payment_information" id="tr_card_number" <?php echo $style; ?>>
+			<label class="<?php echo $controlLabelClass; ?>" for="x_card_num">
+				<?php echo JText::_('AUTH_CARD_NUMBER'); ?><span class="required">*</span>
+			</label>
+
+			<div class="<?php echo $controlsClass; ?>">
+				<input type="text" id="x_card_num" name="x_card_num"
+					   class="input-large validate[required,creditCard]"
+					   value="<?php echo $this->escape($this->input->getAlnum('x_card_num')); ?>" onchange="removeSpace(this);"/>
+			</div>
+		</div>
+		<div class="<?php echo $controlGroupClass;  ?> payment_information" id="tr_exp_date" <?php echo $style; ?>>
+			<label class="<?php echo $controlLabelClass; ?>">
+				<?php echo JText::_('AUTH_CARD_EXPIRY_DATE'); ?><span class="required">*</span>
+			</label>
+
+			<div class="<?php echo $controlsClass; ?>">
+				<?php echo $this->lists['exp_month'] . '  /  ' . $this->lists['exp_year']; ?>
+			</div>
+		</div>
+		<div class="<?php echo $controlGroupClass;  ?> payment_information" id="tr_cvv_code" <?php echo $style; ?>>
+			<label class="<?php echo $controlLabelClass; ?>" for="x_card_code">
+				<?php echo JText::_('AUTH_CVV_CODE'); ?><span class="required">*</span>
+			</label>
+
+			<div class="<?php echo $controlsClass; ?>">
+				<input type="text" id="x_card_code" name="x_card_code"
+					   class="input-large validate[required,custom[number]]"
+					   value="<?php echo $this->escape($this->input->getString('x_card_code')); ?>"/>
+			</div>
+		</div>
+		<?php
+		if ($method->getCardType())
+		{
+			$style = '';
+		}
+		else
+		{
+			$style = ' style = "display:none;" ';
+		}
+		?>
+		<div class="<?php echo $controlGroupClass;  ?> payment_information" id="tr_card_type" <?php echo $style; ?>>
+			<label class="<?php echo $controlLabelClass; ?>" for="card_type">
+				<?php echo JText::_('EB_CARD_TYPE'); ?><span class="required">*</span>
+			</label>
+
+			<div class="<?php echo $controlsClass; ?>">
+				<?php echo $this->lists['card_type']; ?>
+			</div>
+		</div>
+		<?php
+		if ($method->getCardHolderName())
+		{
+			$style = '';
+		}
+		else
+		{
+			$style = ' style = "display:none;" ';
+		}
+		?>
+		<div class="<?php echo $controlGroupClass;  ?> payment_information" id="tr_card_holder_name" <?php echo $style; ?>>
+			<label class="<?php echo $controlLabelClass; ?>" for="card_holder_name">
+				<?php echo JText::_('EB_CARD_HOLDER_NAME'); ?><span class="required">*</span>
+			</label>
+
+			<div class="<?php echo $controlsClass; ?>">
+				<input type="text" id="card_holder_name" name="card_holder_name"
+					   class="input-large validate[required]"
+					   value="<?php echo $this->escape($this->input->getString('card_holder_name')); ?>"/>
+			</div>
+		</div>
+	<?php
+	if ($this->showCaptcha)
+	{
+	?>
+		<div class="<?php echo $controlGroupClass;  ?>">
+			<label class="<?php echo $controlLabelClass; ?>">
+				<?php echo JText::_('EB_CAPTCHA'); ?><span class="required">*</span>
+			</label>
+			<div class="<?php echo $controlsClass; ?>">
+				<?php echo $this->captcha; ?>
+			</div>
+		</div>
+	<?php
+	}
+	?>
+	<div class="form-actions">
+		<input type="button" class="btn btn-primary" name="btnBack" value="<?php echo  JText::_('EB_BACK') ;?>" onclick="window.history.go(-1);" />
+		<input type="submit" class="btn btn-primary" name="btn-submit" id="btn-submit" value="<?php echo JText::_('EB_PROCESS_PAYMENT');?>" />
+		<img id="ajax-loading-animation" src="<?php echo JUri::base(true);?>/media/com_eventbooking/ajax-loadding-animation.gif" style="display: none;"/>
+	</div>
+	<?php
+		if (count($this->methods) == 1)
+		{
+		?>
+			<input type="hidden" name="payment_method" value="<?php echo $this->methods[0]->getName(); ?>" />
+		<?php
+		}
+	?>
+	<input type="hidden" name="Itemid" value="<?php echo $this->Itemid; ?>" />
+	<input type="hidden" name="registrant_id" id="registrant_id" value="<?php echo $this->rowRegistrant->id ; ?>" />
+	<script type="text/javascript">
+		var eb_current_page = 'default';
+		Eb.jQuery(document).ready(function($){
+			$("#adminForm").validationEngine('attach', {
+				onValidationComplete: function(form, status){
+					if (status == true) {
+						form.on('submit', function(e) {
+							e.preventDefault();
+						});
+						form.find('#btn-submit').prop('disabled', true);
+
+						if (typeof stripePublicKey !== 'undefined' && $('#x_card_num').is(":visible"))
+						{
+							if($('input:radio[name^=payment_method]').length)
+							{
+								var paymentMethod = $('input:radio[name^=payment_method]:checked').val();
+							}
+							else
+							{
+								var paymentMethod = $('input[name^=payment_method]').val();
+							}
+
+							if (paymentMethod.indexOf('os_stripe') == 0)
+							{
+								Stripe.card.createToken({
+									number: $('#x_card_num').val(),
+									cvc: $('#x_card_code').val(),
+									exp_month: $('select[name^=exp_month]').val(),
+									exp_year: $('select[name^=exp_year]').val(),
+									name: $('#card_holder_name').val()
+								}, stripeResponseHandler);
+
+								return false;
+							}
+						}
+						return true;
+					}
+					return false;
+				}
+			});
+			buildStateField('state', 'country', '<?php echo $selectedState; ?>');
+		})
+		<?php
+			echo os_payments::writeJavascriptObjects();
+		?>
+		</script>
+		<?php echo JHtml::_( 'form.token' ); ?>
+	</form>
+</div>
