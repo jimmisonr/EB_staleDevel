475c1931366ef1a60d6d2a129b8caf271eb0384f
Tuan Pham Ngoc <github@joomdonation.com>
2016-06-03 14:55:54 +0700

    CS

--- com_eventbooking/site/view/emailtemplates/tmpl/email_cart.php
+++ com_eventbooking/site/view/emailtemplates/tmpl/email_cart.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * @version            2.7.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2016 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2016 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
 defined('_JEXEC') or die;
 ?>
--- com_eventbooking/site/view/emailtemplates/tmpl/group_detail.php
+++ com_eventbooking/site/view/emailtemplates/tmpl/group_detail.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * @version            2.7.0
- * @package        	Joomla
- * @subpackage		Event Booking
- * @author  		Tuan Pham Ngoc
- * @copyright    	Copyright (C) 2010 - 2016 Ossolution Team
- * @license        	GNU/GPL, see LICENSE.php
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2016 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
  */
+
 // no direct access
 defined('_JEXEC') or die;
 $controlGroupClass = $bootstrapHelper->getClassMapping('control-group');
