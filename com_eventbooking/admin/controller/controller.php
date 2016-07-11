bd8ddf191f6eae2d6a9736303844475a587d95b1
Tuan Pham Ngoc <github@joomdonation.com>
2016-07-11 19:11:23 +0700

    Don't clear generated SEF urls on update

--- com_eventbooking/admin/controller/controller.php
+++ com_eventbooking/admin/controller/controller.php
@@ -2120,8 +2120,7 @@ class EventbookingController extends RADControllerAdmin
 				  	";
 		$db->setQuery($sql);
 		$db->execute();
-		$db->truncateTable('#__eb_urls');
-
+		
 		// Migrate waiting list data
 		$sql = 'SELECT COUNT(*) FROM #__eb_waiting_lists';
 		$db->setQuery($sql);
