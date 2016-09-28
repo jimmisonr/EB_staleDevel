89f733e77cf734e2866fbb66e831de32a8d79ae2
Tuan Pham Ngoc <github@joomdonation.com>
2016-09-28 21:22:00 +0700

    Move social sharing buttons to it own lauout

--- com_eventbooking/site/view/event/tmpl/default.php
+++ com_eventbooking/site/view/event/tmpl/default.php
@@ -147,8 +147,8 @@ $offset = JFactory::getConfig()->get('offset');
 				}
 			}
 			?>
-	</div>
-	<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
 	<?php
 	if (!empty($item->ticketTypes))
 	{
@@ -212,68 +212,7 @@ $offset = JFactory::getConfig()->get('offset');
 
 	if ($this->config->show_social_bookmark)
 	{
-		if (empty($this->config->social_sharing_buttons))
-		{
-			$shareOptions = array(
-				'Delicious',
-				'Digg',
-				'Facebook',
-				'Google',
-				'Stumbleupon',
-				'Technorati',
-				'Twitter',
-				'LinkedIn'
-			);
-		}
-		else
-		{
-			$shareOptions = explode(',', $this->config->social_sharing_buttons);
-		}
-	?>
-		<div id="itp-social-buttons-box" class="row-fluid">
-			<div id="eb-share-text"><?php echo JText::_('EB_SHARE_THIS_EVENT'); ?></div>
-			<div id="eb-share-button">
-				<?php
-					$title = $item->title;
-					$html  = '';
-					if (in_array('Delicious', $shareOptions))
-					{
-						$html .= EventbookingHelper::getDeliciousButton($title, $socialUrl);
-					}
-					if (in_array('Digg', $shareOptions))
-					{
-						$html .= EventbookingHelper::getDiggButton($title, $socialUrl);
-					}
-					if (in_array('Facebook', $shareOptions))
-					{
-						$html .= EventbookingHelper::getFacebookButton($title, $socialUrl);
-					}
-					if (in_array('Google', $shareOptions))
-					{
-						$html .= EventbookingHelper::getGoogleButton($title, $socialUrl);
-					}
-					if (in_array('Stumbleupon', $shareOptions))
-					{
-						$html .= EventbookingHelper::getStumbleuponButton($title, $socialUrl);
-					}
-					if (in_array('Technorati', $shareOptions))
-					{
-						$html .= EventbookingHelper::getTechnoratiButton($title, $socialUrl);
-					}
-					if (in_array('Twitter', $shareOptions))
-					{
-						$html .= EventbookingHelper::getTwitterButton($title, $socialUrl);
-					}
-					if (in_array('LinkedIn', $shareOptions))
-					{
-						$html .= EventbookingHelper::getLinkedInButton($title, $socialUrl);
-					}
-
-					echo $html ;
-				?>
-			</div>
-		</div>
-	<?php
+		echo $this->loadTemplate('social_buttons', array('socialUrl' => $socialUrl));
 	}
 ?>
 	</div>
