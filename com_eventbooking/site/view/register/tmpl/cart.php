650fd96dd74b8d81a2176050bfea87220979b57f
Tuan Pham Ngoc <github@joomdonation.com>
2016-10-01 22:02:03 +0700

    Move cart items to it own layout

--- com_eventbooking/site/view/register/tmpl/cart.php
+++ com_eventbooking/site/view/register/tmpl/cart.php
@@ -48,105 +48,18 @@ $layoutData = array(
 	{
 		$msg = $this->message->registration_form_message;
 	}			
-	if (strlen($msg)) 
+
+	if (strlen($msg))
 	{
 		$msg = str_replace('[EVENT_TITLE]', $this->eventTitle, $msg) ;
-		$msg = str_replace('[AMOUNT]', EventBookingHelper::formatCurrency($this->amount, $this->config), $msg) ;			
+		$msg = str_replace('[AMOUNT]', EventbookingHelper::formatCurrency($this->amount, $this->config), $msg) ;
 	?>								
 		<div class="eb-message"><?php echo $msg ; ?></div>							 															
 	<?php	
 	}
+
+	echo $this->loadTemplate('items');
 ?>
-<div class="eb-cart-content">		
-	<table class="table table-striped table-bordered table-condensed">
-		<thead>
-			<tr>			
-				<th class="col_event">
-					<?php echo JText::_('EB_EVENT'); ?>
-				</th>
-				<?php
-					if ($this->config->show_event_date) {
-					?>
-						<th class="col_event_date">
-							<?php echo JText::_('EB_EVENT_DATE'); ?>
-						</th>	
-					<?php	
-					}
-				?>																	
-				<th class="col_price">
-					<?php echo JText::_('EB_PRICE'); ?>
-				</th>					
-				<th class="col_quantity">
-					<?php echo JText::_('EB_QUANTITY'); ?>
-				</th>
-				<th class="col_subtotal">
-					<?php echo JText::_('EB_SUB_TOTAL'); ?>
-				</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php			
-			$total = 0 ;
-			$k = 0 ;			
-			for ($i = 0 , $n = count($this->items) ; $i < $n; $i++) 
-			{
-				$item = $this->items[$i] ;				
-				$total += $item->quantity*$item->rate ;				
-	        	$url = JRoute::_('index.php?option=com_eventbooking&view=event&id='.$item->id.'&tmpl=component&Itemid='.$this->Itemid);
-			?>
-				<tr>								
-					<td class="col_event">
-						<a href="<?php echo $url; ?>" <?php echo $popup; ?>><?php echo $item->title; ?></a>								
-					</td>	
-					<?php
-						if ($this->config->show_event_date) 
-						{
-						?>
-							<td class="col_event_date">
-								<?php
-                                    if ($item->event_date == EB_TBC_DATE) 
-									{
-                                        echo JText::_('EB_TBC');
-                                    } 
-                                    else 
-									{
-                                        echo JHtml::_('date', $item->event_date, $this->config->event_date_format, null); 
-                                    }
-								?>								
-							</td>
-						<?php	
-						}
-					?>																				
-					<td class="col_price">
-						<?php echo EventbookingHelper::formatCurrency($item->rate, $this->config); ?>
-					</td>
-					<td class="col_quantity">
-						<?php echo $item->quantity ; ?>
-					</td>	
-					<td class="col_price">
-						<?php echo EventbookingHelper::formatCurrency($item->rate*$item->quantity, $this->config); ?>
-					</td>					
-				</tr>
-			<?php				
-				$k = 1 - $k ;				
-			}				
-			if ($this->config->show_event_date) 
-			{
-				$cols = 5 ;
-			} 
-			else 
-			{
-				$cols = 4 ;
-			}										
-			?>		
-			<tr>				
-				<td colspan="<?php echo $cols ; ?>" style="text-align: right;">										
-					<input type="button" class="btn" value="<?php echo JText::_('EB_MODIFY_CART'); ?>" onclick="updateCart();" />																									
-				</td>								
-			</tr>		
-		</tbody>		
-	</table>				
-</div>
 <div class="clearfix"></div>	
 <?php
 	if (!$this->userId && $this->config->user_registration)
