9ac05bd8143ce423f85e8e417c2d3789692bbb56
Tuan Pham Ngoc <github@joomdonation.com>
2016-10-01 17:25:07 +0700

    Move use registration form to common layout - kill repeating code

--- com_eventbooking/site/view/register/tmpl/cart.php
+++ com_eventbooking/site/view/register/tmpl/cart.php
@@ -164,36 +164,14 @@ $controlsClass     = $bootstrapHelper->getClassMapping('controls');
 	<?php	 									
 		if (!$this->userId && $this->config->user_registration) 
 		{
-			$params = JComponentHelper::getParams('com_users');
-			$minimumLength = $params->get('minimum_length', 4);
-			($minimumLength) ? $minSize = ",minSize[$minimumLength]" : $minSize = "";
-		?>						
-		<div class="<?php echo $controlGroupClass;  ?>">
-			<label class="<?php echo $controlLabelClass; ?>" for="username1">
-				<?php echo  JText::_('EB_USERNAME') ?><span class="required">*</span>
-			</label>
-			<div class="<?php echo $controlsClass; ?>">      				
-				<input type="text" name="username" id="username1" class="input-large validate[required,minSize[2],ajax[ajaxUserCall]]" value="<?php echo $this->escape($this->input->getUsername('username')); ?>" />
-				<span class="invalid" id="validate_username_msg" style="display: none;"><?php echo JText::_('EB_INVALID_USERNAME'); ?></span>		
-			</div>	
-		</div>					
-		<div class="<?php echo $controlGroupClass;  ?>">			
-			<label class="<?php echo $controlLabelClass; ?>" for="password1">					
-				<?php echo  JText::_('EB_PASSWORD') ?><span class="required">*</span>						
-			</label>				
-			<div class="<?php echo $controlsClass; ?>">
-				<input type="password" name="password1" id="password1" class="input-large validate[required<?php echo $minSize;?>]" value=""/>
-			</div>
-		</div>
-		<div class="<?php echo $controlGroupClass;  ?>">			
-			<label class="<?php echo $controlLabelClass; ?>" for="password2">					
-				<?php echo  JText::_('EB_RETYPE_PASSWORD') ?><span class="required">*</span>					
-			</label>				
-			<div class="<?php echo $controlsClass; ?>">
-				<input type="password" name="password2" id="password2" class="input-large validate[required,equals[password1]]" value="" />					
-			</div>
-		</div>											
-		<?php	
+			$layoutData = array(
+				'username'          => $this->input->getUsername('username'),
+				'controlGroupClass' => $controlGroupClass,
+				'controlLabelClass' => $controlLabelClass,
+				'controlsClass'     => $controlsClass,
+			);
+
+			echo $this->loadCommonLayout('register/tmpl/register_user_registration.php', $layoutData);
 		}
 
 		// Collect registrants information
--- com_eventbooking/site/view/register/tmpl/group_billing.php
+++ com_eventbooking/site/view/register/tmpl/group_billing.php
@@ -50,45 +50,28 @@ else
 <?php
 	if (!$this->userId && $this->config->user_registration)
 	{
-		$params = JComponentHelper::getParams('com_users');
-		$minimumLength = $params->get('minimum_length', 4);
-		($minimumLength) ? $minSize = ",minSize[$minimumLength]" : $minSize = "";
-	?>
-	<div class="<?php echo $controlGroupClass; ?>">
-		<label class="<?php echo $controlLabelClass; ?>" for="username1">
-			<?php echo  JText::_('EB_USERNAME') ?><span class="required">*</span>
-		</label>
-		<div class="<?php echo $controlsClass; ?>">
-			<input type="text" name="username" id="username1" class="input-large validate[required,minSize[2],ajax[ajaxUserCall]]" value="<?php echo $this->escape($this->input->getUsername('username')); ?>"/>
-		</div>
-	</div>
-	<div class="<?php echo $controlGroupClass; ?>">
-		<label class="<?php echo $controlLabelClass; ?>" for="password1">
-			<?php echo  JText::_('EB_PASSWORD') ?><span class="required">*</span>
-		</label>
-		<div class="<?php echo $controlsClass; ?>">
-			<input type="password" name="password1" id="password1" class="input-large input-large validate[required<?php echo $minSize;?>]" value=""/>
-		</div>
-	</div>
-	<div class="<?php echo $controlGroupClass; ?>">
-		<label class="<?php echo $controlLabelClass; ?>" for="password2">
-			<?php echo  JText::_('EB_RETYPE_PASSWORD') ?><span class="required">*</span>
-		</label>
-		<div class="<?php echo $controlsClass; ?>">
-			<input type="password" name="password2" id="password2" class="input-large validate[required,equals[password1]]" value=""/>
-		</div>
-	</div>
-	<?php
+		$layoutData = array(
+			'username'          => $this->input->getUsername('username'),
+			'controlGroupClass' => $controlGroupClass,
+			'controlLabelClass' => $controlLabelClass,
+			'controlsClass'     => $controlsClass,
+		);
+
+		echo $this->loadCommonLayout('register/tmpl/register_user_registration.php', $layoutData);
 	}
+
 	$fields = $this->form->getFields();
+
 	if (isset($fields['state']))
 	{
 		$selectedState = $fields['state']->value;
 	}
+
 	$dateFields = array();
 	foreach ($fields as $field)
 	{
 		echo $field->getControlGroup($bootstrapHelper);
+
 		if ($field->type == "Date")
 		{
 			$dateFields[] = $field->name;
