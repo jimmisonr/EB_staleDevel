6982364f01fc138f8d801a5ceac2240e870e769d
Tuan Pham Ngoc <github@joomdonation.com>
2016-10-05 14:57:54 +0700

    Load custom css in upcoming events module

--- modules/mod_eb_events/mod_eb_events.php
+++ modules/mod_eb_events/mod_eb_events.php
@@ -13,6 +13,7 @@ defined('_JEXEC') or die;
 require_once JPATH_ROOT . '/components/com_eventbooking/helper/helper.php';
 require_once JPATH_ROOT . '/components/com_eventbooking/helper/route.php';
 require_once JPATH_ROOT . '/components/com_eventbooking/helper/jquery.php';
+
 $user   = JFactory::getUser();
 $config = EventbookingHelper::getConfig();
 
@@ -24,16 +25,19 @@ else
 {
 	error_reporting(0);
 }
+
 $document = JFactory::getDocument();
 EventbookingHelper::loadLanguage();
 $db      = JFactory::getDbo();
 $query   = $db->getQuery(true);
 $baseUrl = JUri::base(true);
 $itemId  = (int) $params->get('item_id', 0);
+
 if (!$itemId)
 {
 	$itemId = EventbookingHelper::getItemid();
 }
+
 $fieldSuffix  = EventbookingHelper::getFieldSuffix();
 $currentDate  = $db->quote(JHtml::_('date', 'Now', 'Y-m-d'));
 $numberEvents = $params->get('number_events', 6);
@@ -75,8 +79,8 @@ if (JFactory::getApplication()->getLanguageFilter())
 $db->setQuery($query, 0, $numberEvents);
 $rows = $db->loadObjectList();
 
-$query->clear();
-$query->select('a.id, a.name' . $fieldSuffix . ' AS name')
+$query->clear()
+	->select('a.id, a.name' . $fieldSuffix . ' AS name')
 	->from('#__eb_categories AS a')
 	->innerJoin('#__eb_event_categories AS b ON a.id = b.category_id');
 
@@ -87,20 +91,25 @@ for ($i = 0, $n = count($rows); $i < $n; $i++)
 	$db->setQuery($query);
 	$categories             = $db->loadObjectList();
 	$row->number_categories = count($categories);
+
 	if (count($categories))
 	{
 		$itemCategories = array();
+
 		foreach ($categories as $category)
 		{
 			$itemCategories[] = '<a href="' . EventbookingHelperRoute::getCategoryRoute($category->id, $itemId) . '"><strong>' . $category->name .
 				'</strong></a>';
 		}
+
 		$row->categories = implode('&nbsp;|&nbsp;', $itemCategories);
 	}
+
 	$query->clear('where');
 }
 
 $layout = $params->get('layout', 'default');
+
 if ($layout == 'default')
 {
 	$document->addStyleSheet($baseUrl . '/modules/mod_eb_events/css/style.css');
@@ -111,8 +120,10 @@ else
 	{
 		$document->addStyleSheet($baseUrl . '/media/com_eventbooking/assets/bootstrap/css/bootstrap.css');
 	}
+
 	$document->addStyleSheet($baseUrl . '/modules/mod_eb_events/css/improved.css');
 }
+
 if ($config->calendar_theme)
 {
 	$theme = $config->calendar_theme;
@@ -123,4 +134,10 @@ else
 }
 
 $document->addStyleSheet($baseUrl . '/media/com_eventbooking/assets/css/themes/' . $theme . '.css');
+
+if (file_exists(JPATH_ROOT . '/media/com_eventbooking/assets/css/custom.css') && filesize(JPATH_ROOT . '/media/com_eventbooking/assets/css/custom.css') > 0)
+{
+	$document->addStyleSheet($baseUrl . '/media/com_eventbooking/assets/css/custom.css');
+}
+
 require JModuleHelper::getLayoutPath('mod_eb_events', $layout);
