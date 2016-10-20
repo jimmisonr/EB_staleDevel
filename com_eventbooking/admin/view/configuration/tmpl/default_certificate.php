4798fbbfcedaafcfdfabb441c7a540868b20abb5
Tuan Pham Ngoc <github@joomdonation.com>
2016-10-20 16:13:53 +0700

    Split Configuration Page to smaller layouts, easier to maintain and modify if needed

---
+++ com_eventbooking/admin/view/configuration/tmpl/default_certificate.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package            Joomla
+ * @subpackage         Event Booking
+ * @author             Tuan Pham Ngoc
+ * @copyright          Copyright (C) 2010 - 2016 Ossolution Team
+ * @license            GNU/GPL, see LICENSE.php
+ */
+// no direct access
+defined( '_JEXEC' ) or die ;
