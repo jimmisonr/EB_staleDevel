b415d8c5467097f5715a56ab0ddf0ac35ba68317
Tuan Pham Ngoc <github@joomdonation.com>
2016-11-23 14:53:30 +0700

    Remove those settings, it should belong to event settings

--- com_eventbooking/admin/view/configuration/tmpl/default_tickets.php
+++ com_eventbooking/admin/view/configuration/tmpl/default_tickets.php
@@ -19,22 +19,6 @@ defined( '_JEXEC' ) or die ;
 </div>
 <div class="control-group">
 	<div class="control-label">
-		<?php echo EventbookingHelperHtml::getFieldLabel('ticket_start_number', JText::_('EB_TICKET_START_NUMBER'), JText::_('EB_TICKET_START_NUMBER_EXPLAIN')); ?>
-	</div>
-	<div class="controls">
-		<input type="text" name="invoice_start_number" class="inputbox" value="<?php echo $config->ticket_start_number ? $config->ticket_start_number : 1; ?>" size="10" />
-	</div>
-</div>
-<div class="control-group">
-	<div class="control-label">
-		<?php echo EventbookingHelperHtml::getFieldLabel('ticket_prefix', JText::_('EB_TICKET_PREFIX'), JText::_('EB_TICKET_PREFIX_EXPLAIN')); ?>
-	</div>
-	<div class="controls">
-		<input type="text" name="ticket_prefix" class="inputbox" value="<?php echo $config->get('ticket_prefix', 'TK'); ?>" size="10" />
-	</div>
-</div>
-<div class="control-group">
-	<div class="control-label">
 		<?php echo EventbookingHelperHtml::getFieldLabel('ticket_number_length', JText::_('EB_TICKET_NUMBER_LENGTH'), JText::_('EB_TICKET_NUMBER_LENGTH_EXPLAIN')); ?>
 	</div>
 	<div class="controls">
