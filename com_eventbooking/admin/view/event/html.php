c3f64b03c31e8d912c69190e7b1f6248d419dc5b
Tuan Pham Ngoc <github@joomdonation.com>
2017-04-12 16:24:54 +0700

    Fix terms and conditions

--- com_eventbooking/admin/view/event/html.php
+++ com_eventbooking/admin/view/event/html.php
@@ -245,7 +245,7 @@ class EventbookingViewEventHtml extends RADViewItem
 		$options[] = JHtml::_('select.option', 1, JText::_('JYES'));
 		$options[] = JHtml::_('select.option', 2, JText::_('EB_USE_GLOBAL_CONFIGURATION'));
 
-		$this->lists['enable_terms_and_conditions'] = JHtml::_('select.booleanlist', 'enable_terms_and_conditions', ' class="inputbox" ', $item->enable_terms_and_conditions);
+		$this->lists['enable_terms_and_conditions'] = JHtml::_('select.genericlist', $options, 'enable_terms_and_conditions', ' class="inputbox" ', 'value', 'text', $item->enable_terms_and_conditions);
 		
 		#Plugin support
 		JPluginHelper::importPlugin('eventbooking');
